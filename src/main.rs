2e591792f1a4c5d36fc32d48c00ebcc7628a5cf1
Michal Vala <michal.vala@gmail.com>
2022-05-29 09:44:36 +0200

    better draw, npc object

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use tcod::colors::*;
 use tcod::console::*;
-use roguelike::player::Player;
+use roguelike::player::Object;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -9,6 +9,7 @@ const LIMIT_FPS: i32 = 20;
 
 struct Tcod {
     root: Root,
+    con: Offscreen,
 }
 
 fn main() {
@@ -18,30 +19,41 @@ fn main() {
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Roguelike")
         .init();
-    let mut tcod = Tcod { root };
+    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let mut tcod = Tcod { root, con };
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player = Player::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+    let npc = Object::new(3, 3, '#', YELLOW);
+
+    let mut objects = [player, npc];
     while !tcod.root.window_closed() {
-        tcod.root.set_default_foreground(WHITE);
-        tcod.root.clear();
-        tcod.root.put_char(player.x, player.y, '@', BackgroundFlag::None);
+        tcod.con.clear();
+
+        for o in &objects {
+            o.draw(&mut tcod.con);
+        }
+
+        blit(&tcod.con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut tcod.root, (0, 0), 1.0, 1.0);
         tcod.root.flush();
-        let exit = handle_keys(&mut tcod, &mut player);
+
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, player);
+
         if exit { break; }
     }
 }
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Player) -> bool {
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
     let key = tcod.root.wait_for_keypress(true);
     match key {
-        Key { code: Up, .. } => player.y -= 1,
-        Key { code: Down, .. } => player.y += 1,
-        Key { code: Left, .. } => player.x -= 1,
-        Key { code: Right, .. } => player.x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
         Key { code: Enter, alt: true, .. } => tcod.root.set_fullscreen(!tcod.root.is_fullscreen()),
         Key { code: Escape, .. } => return true,
         _ => {}
--- src/player.rs
+++ src/player.rs
@@ -1,13 +1,29 @@
-pub struct Player {
-    pub x: i32,
-    pub y: i32,
+use tcod::{BackgroundFlag, Color, Console};
+
+pub struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
 }
 
-impl Player {
-    pub fn new(x: i32, y: i32) -> Self {
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
         Self {
             x,
             y,
+            char,
+            color,
         }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
     }
 }
